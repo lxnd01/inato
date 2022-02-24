6bac68064d6d250c09838df787f5fa5b25b585ee
lxnd <alexandre.petit@vroomly.com>
2022-02-24 22:17:28 +0100

    refactor: use affirmative assertions

--- pharmacy.js
+++ pharmacy.js
@@ -49,7 +49,11 @@ export class Pharmacy {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
+        if (this.drugs[i].name === "Herbal Tea") {
+          if (this.drugs[i].benefit < 50) {
+            this.drugs[i].increaseBenefit();
+          }
+        } else {
           if (this.drugs[i].name != "Fervex") {
             if (this.drugs[i].benefit > 0) {
               if (this.drugs[i].name != "Magic Pill") {
@@ -59,10 +63,6 @@ export class Pharmacy {
           } else {
             this.drugs[i].benefit =
               this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].increaseBenefit();
           }
         }
       }
