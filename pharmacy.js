6317acd320cc51514ed03b43b2ad7aba7791df73
lxnd <alexandre.petit@vroomly.com>
2022-02-24 23:03:40 +0100

    refactor: cleanup the fervex logic

--- pharmacy.js
+++ pharmacy.js
@@ -84,23 +84,15 @@ export class Fervex extends Drug {
 
   updateBenefitValue() {
     this.increaseBenefit();
-    if (this.name === "Fervex") {
-      if (this.expiresIn < 11) {
-        this.increaseBenefit();
-      }
-      if (this.expiresIn < 6) {
-        this.increaseBenefit();
-      }
+    if (this.expiresIn < 11) {
+      this.increaseBenefit();
+    }
+    if (this.expiresIn < 6) {
+      this.increaseBenefit();
     }
     this.expiresIn = this.expiresIn - 1;
     if (this.expired()) {
-      if (this.name === "Herbal Tea") {
-        this.increaseBenefit();
-      } else {
-        if (this.name === "Fervex") {
-          this.benefit = this.benefit - this.benefit;
-        }
-      }
+      this.benefit = this.benefit - this.benefit;
     }
   }
 }
